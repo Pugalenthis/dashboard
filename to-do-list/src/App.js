60763f830e661662890262e86fedbc7a9957ced4
pugalenthi <aravindpugal505@gmail.com>
2022-01-31 13:31:17 +0800

    moviesnew

---
+++ to-do-list/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+         
+    </div>
+  );
+}
+
+function Tdlist() {
+  return (
+      <div>
+         
+      </div>
+  )
+}
+
+export default App;
+
+
+function user(){
+  let input = document.getElementById("userinput").value
+  console.log(input)
+  document.getElementById("para").innerHTML=document.getElementById("userinput").value
+
+}
