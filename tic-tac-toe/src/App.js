11b6bf6c4624ac897682e071e8a760228be48a1b
pugalenthi <aravindpugal505@gmail.com>
2022-02-07 09:58:50 +0530

    first commit

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,123 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [board, setboard] = useState([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  // useState([0,1,2,3,4,5,6,7,8])
+
+  const decidewinner = (board) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (var i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      //  console.log(a,b,c);
+      if (
+        (board[a] !== null) & (board[a] === board[b]) &&
+        board[b] === board[c]
+      ) {
+        //  console.log("winner is ",board[a])
+        return board[a];
+      }
+    }
+    return null;
+  };
+  const winner = decidewinner(board);
+
+  const [isXturn, setXturn] = useState(true);
+
+  const handleclick = (index) => {
+    // console.log(index);
+    if (winner === null && board[index] === null) {
+      const boardcopy = [...board];
+      boardcopy[index] = isXturn ? "x" : "o";
+    //   boardcopy[index] = isXturn ? "x" : "o";
+      // console.log(boardcopy);
+      setboard(boardcopy);
+      setXturn(!isXturn);
+    }
+  };
+  return (
+    <div className="App">
+      <div className="container">
+        <button className="btn  player-x-button" onClick={()=>{
+              setXturn(true)
+        }}>player X</button>
+        <a href="#"><button className="btn  player-o-button" onClick={()=>{
+              setXturn(false)
+        }}>player O</button></a>
+        <div className="row" id="gamebox-row">
+          <div class="full-game">
+            <div className="board">
+              {board.map((val, index) => (
+                <Gamebox val={val} onplayerclick={() => handleclick(index)} />
+              ))}
+            </div>
+
+            <div>{winner ? <h2>winner is ;{winner}</h2> : ""}</div>
+            <div>
+              <button
+                className="btn btn-success restart-button"
+                onClick={() => {
+                  setboard([
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                  ]);
+                }}
+              >
+                Restart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+function Gamebox({ val, onplayerclick }) {
+  const styles = {
+    color: val === "x" ? "green" : "red",
+  };
+  // const [val,setval]=useState(null)
+  return (
+    <div>
+      <div style={styles} onClick={() => onplayerclick()} className="colorbox">
+        {val}
+      </div>
+    </div>
+  );
+}
+
+// draw condition
+// tell whose turn
+// player to start x or o
+// restart
--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,28 +1,62 @@
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
+  const tasks = ["Have meeting at feb 12", "Have class at morning 10am"]
+
+
+  const [copytask, settask] = useState(tasks)
+  const [usertask, setusertask] = useState()
+ 
   return (
     <div className="App">
-         
+      <div className='container'>
+        <div className='row'>
+          <div className="col-12">
+            <div className="heading">
+              <h1 >TO DO LIST</h1>
+            </div>
+            <div className="input-group mb-3">
+              <input placeholder='Enter your new task......' type="text" onChange={(event) => {
+                setusertask(event.target.value)
+                console.log(usertask)
+              }}  className="form-control"></input>
+              <button onClick={() => {
+                   settask([...copytask,usertask])
+                   console.log(copytask)
+                   console.log(usertask)
+                  
+              }} className="btn bg-success btn-outline-secondary" type="button" id="button-addon2">Add new task</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      {copytask.map((event)=><Tdlist task= {event}/>)}x
     </div>
   );
 }
 
-function Tdlist() {
+function Tdlist({ task }) {
+  const[currentdisplay,setdisplay] = useState(true)
+  // const styles = {
+  //    display : currentdisplay ? "block" : "none",
+  // }
+ 
   return (
-      <div>
-         
-      </div>
+    <div className='container'>
+      <table class="table table-dark">
+        <tbody>
+          <tr>
+            {currentdisplay ?<td ><bold>{task}</bold><button onClick={()=>setdisplay(false)} className='btn bg-danger' >Remove task</button></td> : ""}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
 export default App;
 
 
-function user(){
-  let input = document.getElementById("userinput").value
-  console.log(input)
-  document.getElementById("para").innerHTML=document.getElementById("userinput").value
-
-}
